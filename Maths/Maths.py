30fbced6339465425a37817230f8766372aef44d
EtienneQuarteyPapafio <etiennequartey@gmail.com>
2025-06-05 01:02:40 +0100

    New

---
+++ Maths/Maths.py
@@ -0,0 +1,16 @@
+#you can do maths with python
+
+#numbers can be stored in variables
+
+pi=3.14159
+print(pi)
+
+#and arithmetic 
+
+print(pi+pi, " adding")
+print(pi-pi, " subtracting")
+print(pi*pi, " multiplying")
+print(pi/pi, " dividing")
+print(pi**pi, " exponenting")
+
+#however if you start combining data types python can get confused
